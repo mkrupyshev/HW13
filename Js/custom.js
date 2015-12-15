bb723958d2c709bc9f5ba27e94b53814e80d82f0
Rustam Gasanov <rustamagasanov@gmail.com>
2015-12-15 23:04:56 +0100

    :shipit:

--- Js/custom.js
+++ Js/custom.js
@@ -2,20 +2,20 @@ $(function() {
   $("#playpause").click(function(){
     if($("#video")[0].paused) {
       $("#video").trigger("play")
-        $("#playpauseicon").attr("class", "fa fa-pause")
+      $("#playpauseicon").attr("class", "fa fa-pause")
     } else {
       $("#video").trigger("pause")
-        $("#playpauseicon").attr("class", "fa fa-play")
+      $("#playpauseicon").attr("class", "fa fa-play")
     }
   })
 
   $("#muteunmute").click(function(){
     if($("#video")[0].muted) {
       video.muted = false
-        $("#muteunmuteicon").attr("class", "fa fa-volume-off")
+      $("#muteunmuteicon").attr("class", "fa fa-volume-off")
     } else {
       video.muted = true
-        $("#muteunmuteicon").attr("class", "fa fa-volume-up")
+      $("#muteunmuteicon").attr("class", "fa fa-volume-up")
     }
   })
 })
